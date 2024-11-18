568eb8f3e00f3bd6e95c95a2362e39db7a94d308
Hossein <hoseinshahmohammady@gmail.com>
2024-11-18 09:48:39 -0800

    initial commit.

--- image_optimizer/settings.py
+++ image_optimizer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-+1__%tl917pru66z=(xe^gpoex3woh8+hji1h#ozg-3g-%3s^f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0','172.105.38.184']
+ALLOWED_HOSTS = ['172.105.38.184']
 
 
 # Application definition
