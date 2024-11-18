075fb5295dee08e55e17776d8df655660cd4921b
Hossein <hoseinshahmohammady@gmail.com>
2024-11-18 05:45:24 -0800

    Add docker-compose.yml and related configuration files

--- image_optimizer/settings.py
+++ image_optimizer/settings.py
@@ -78,18 +78,20 @@ WSGI_APPLICATION = 'image_optimizer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD':'b9?egXS.Ksj8#%1qa-1yP',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+DATABASES = {     
+    'default':
+     {
+        'ENGINE': 'django.db.backends.postgresql',         
+        'NAME': 'postgres',         
+        'USER': 'postgres',         
+        'PASSWORD':'b9?egXS.Ksj8#%1qa-1yP',         
+        'HOST': 'db',         
+        'PORT': '5432',    
+        
+    } 
+         
+        }
 
-# 'b9?egXS.Ksj8#%1qa-1yP'
 
 
 
