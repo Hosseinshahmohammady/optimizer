5463234e612cc9b813bef528642b77d1ae3298a2
Hossein <hoseinshahmohammady@gmail.com>
2024-12-07 03:27:43 -0800

    initial commit.

--- image_optimizer/urls.py
+++ image_optimizer/urls.py
@@ -20,9 +20,30 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Image Optimizer API",
+        default_version='v1',
+        description="API for optimizing and reducing image size",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@hoseinshahmohammady.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('optimizer.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('swagger.yaml', schema_view.without_ui(cache_timeout=0).as_view(), name='schema-swagger-yaml'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
