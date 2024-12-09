260cd9a9615f14e0bba1cd657dda93f8ff5bda8e
Hossein <hoseinshahmohammady@gmail.com>
2024-12-09 07:13:20 -0800

    initial commit.

--- optimizer/urls.py
+++ optimizer/urls.py
@@ -6,6 +6,8 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.http import HttpResponse
+from rest_framework import routers
+# from rest_framework_swagger.views import get_swagger_view
 
 
 schema_view = get_schema_view(
@@ -23,11 +25,11 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
+path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+path('swagger.yaml', schema_view.without_ui(cache_timeout=0), name='swagger-yaml'),
 path('optimize/', optimize_image, name='optimize_image'),
 path('show_image/<int:pk>/', views.show_image, name='show_image'),
 # path('image/<int:pk>/', views.show_image, name='show_image'),
 path('<int:pk>/', views.image_id, name='url_id'),
-path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-path('swagger.yaml', schema_view.without_ui(cache_timeout=0), name='swagger-yaml'),
 
 ]
