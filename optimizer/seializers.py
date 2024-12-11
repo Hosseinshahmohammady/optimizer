f3b20385ccf9c0b7cca651319cd1e3b62ec1113f
Hossein <hoseinshahmohammady@gmail.com>
2024-12-11 01:11:02 -0800

    initial commit.

--- optimizer/seializers.py
+++ optimizer/seializers.py
@@ -2,8 +2,7 @@ from rest_framework import serializers
 
 class ImageUploadSerializer(serializers.Serializer):
     image = serializers.ImageField(required=True)
-    quality = serializers.IntegerField(
-        required=True, 
+    quality = serializers.IntegerField( 
         min_value=1, 
         max_value=100, 
         default=85
