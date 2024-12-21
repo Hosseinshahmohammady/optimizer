518bac3837464fd301752ee178960207f0bd92f1
Hossein <hoseinshahmohammady@gmail.com>
2024-12-21 05:20:05 -0800

    initial commit.

--- optimizer/views.py
+++ optimizer/views.py
@@ -43,13 +43,13 @@ def login_view(request):
         form = LoginForm(request=request, data= request.POST)
         if form.is_valid():
              username = form.cleaned_data.get('username')
-             password = form.cleaned_data.get('passworod')
+             password = form.cleaned_data.get('password')
              user = authenticate(username=username, password=password)
              if user is not None:
                   login(request, user)
                   return redirect('swagger')
              else :
-                  return render(request, 'login.html', {'form': form})
+                  form.add_error(None, "Invalid login credentials.")
         else :
              form = LoginForm()
         return render(request, 'login.html', {'form': form})
