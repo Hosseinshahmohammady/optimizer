6f33330e6dd40291d6e4d6a99042cbc45c317b9c
Hossein <hoseinshahmohammady@gmail.com>
2025-01-21 02:39:26 -0800

    initial commit.

--- optimizer/seializers.py
+++ optimizer/seializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
 
-# class ImageUploadSerializer(serializers.Serializer):
-#     image = serializers.ImageField(required=True)
-#     image2 = serializers.ImageField(required=False)
-#     quality = serializers.IntegerField(required=False, min_value=1, max_value=100, default=95)
+class ImageUploadSerializer(serializers.Serializer):
+    image = serializers.ImageField(required=True)
+    image2 = serializers.ImageField(required=False)
+    quality = serializers.IntegerField(required=False, min_value=1, max_value=100, default=95)
 #     width = serializers.IntegerField(required=False, min_value=1)
 #     height = serializers.IntegerField(required=False, min_value=1)
 #     grayscale = serializers.BooleanField(required=False, default=False)
@@ -29,10 +29,10 @@ from rest_framework import serializers
 #     panorama_image = serializers.BooleanField(required=False, default=False)
 
 
-    # def validate_quality(self, value):
-    #     if value is None:
-    #         raise serializers.ValidationError("Quality is required")
-    #     if value < 1 or value > 100:
-    #         raise serializers.ValidationError("Quality must be between 1 and 100")
-    #     return value
+    def validate_quality(self, value):
+        if value is None:
+            raise serializers.ValidationError("Quality is required")
+        if value < 1 or value > 100:
+            raise serializers.ValidationError("Quality must be between 1 and 100")
+        return value
 
--- optimizer/views.py
+++ optimizer/views.py
@@ -11,9 +11,10 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from drf_yasg.utils import swagger_auto_schema
-# from .seializers import ImageUploadSerializer
+from .seializers import ImageUploadSerializer
 import os
 import cv2
+from PIL import Image
 import numpy as np
 from .forms import SignUpForm, LoginForm
 from django.shortcuts import render, redirect
@@ -112,6 +113,65 @@ class OptimizeImageView(APIView):
             return Response({'error': 'Invalid data'}, status=status.HTTP_400_BAD_REQUEST)
      
         
+        image = serializer.validated_data.get('image')
+        image2 = serializer.validated_data.get('image2')
+        if not image and not image2:
+         return Response({'error': 'At least one image must be provided'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            if image:
+                file_data = image.read()
+                nparr = np.frombuffer(file_data, np.uint8)
+                img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+                if img is None:
+                    raise ValueError("The image could not be decoded.")
+            else:
+                img = None
+
+            if image2:
+                file_data2 = image2.read()
+                nparr2 = np.frombuffer(file_data2, np.uint8)
+                img2 = cv2.imdecode(nparr2, cv2.IMREAD_COLOR)
+                if img2 is None:
+                    raise ValueError("The second image could not be decoded.")
+            else:
+                img2 = None
+
+        except Exception as e:
+            return Response({'error': str(e)}, status=400)
+
+        quality = serializer.validated_data.get('quality')
+        try:
+            quality = int(quality)
+            if quality < 1 or quality > 100:
+                return JsonResponse({'error': 'Quality must be between 1 and 100'}, status=400)
+        except ValueError:
+            return JsonResponse({'error': 'Quality must be a valid integer'}, status=400)
+
+
+        image = Image.open(image)
+
+        media_path = settings.MEDIA_ROOT
+        if not os.path.exists(media_path):
+            os.makedirs(media_path)
+
+        existing_files = os.listdir(media_path)
+        pk = len(existing_files) + 1
+
+        file_name = f'id={pk}.jpg'
+        file_path = os.path.join(media_path, file_name)
+
+        image.save(file_path, format='JPEG', quality=quality)
+
+        image_url = os.path.join(settings.MEDIA_URL, file_name)
+        #  short_url = f"{settings.SITE_URL}/image/{pk}"
+
+        return JsonResponse({
+            'message': 'Image optimized and saved',
+             'image_url': image_url,
+            # 'short_url': short_url,
+             'image_id': 'Enter your browser : http://172.105.38.184:8000/api/pk/'
+        }) 
 
 
 
@@ -172,4 +232,5 @@ def image_id(request, pk):
     return Response({
         'message': 'Image found',
         'image_url': image_url,
-    })
+     })
+
